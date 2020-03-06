cb4c5cfa8b14758d8abc37d19f13881695579b2e
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-03-06 10:58:52 +0000

    finist to add content and make some style

--- web/js/ajax-call.js
+++ web/js/ajax-call.js
@@ -1,10 +1,14 @@
+const appWillRenderHere = document.querySelector('#view')
+const ce = tag => document.createElement(tag)
+
+
 const getResponse = lala => {
   const ajaxCall = new XMLHttpRequest()
 
   ajaxCall.onreadystatechange = () => {
     if(ajaxCall.readyState === 4 && ajaxCall.status === 200) {
       const response = JSON.parse(ajaxCall.responseText)
-      console.log(response)
+      render(response)
     }
   }
 
@@ -16,5 +20,42 @@ const pathList = ["animals", "weather", "food"]
 pathList.forEach(path => {
   getResponse(path)
  })
+
+const render = response => {
+  console.table(response[0])
+  const keyList = Object.keys(response[0])
+  console.log(keyList)
+
+  const table = ce('table')
+  const tableHead = ce('thead')
+  const tableBody = ce('tbody')
+  const tableHeadRow = ce('tr')
+  keyList.forEach(key => {
+    const td = ce('td')
+    td.textContent = key
+    tableHeadRow.appendChild(td)
+  })
+
+  response.forEach(myObject => {
+    const tableBodyRow = ce('tr')
+    keyList.forEach(key => {
+      const td = ce('td')
+      td.textContent = myObject[key]
+      tableBodyRow.appendChild(td)
+    })
+    tableBody.appendChild(tableBodyRow)
+  })
+  
+  
+  tableHead.appendChild(tableHeadRow)
+  table.appendChild(tableHead)
+  
+  table.appendChild(tableBody)
+  appWillRenderHere.appendChild(table)
+  appWillRenderHere.className = 'view'
+  
+}
+
+
 
 
